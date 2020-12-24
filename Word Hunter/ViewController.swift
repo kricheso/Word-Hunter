659a9b7bc8055191775f1d2aee55d516ef03946e
Kousei Richeson <kousei.richeson@gmail.com>
2020-12-24 16:10:37 -0700

    Initial Commit

---
+++ Word Hunter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Hunter
+//
+//  Created by Kousei Richeson on 12/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
